c6290ca9cac636c393c5c9f4cbf4ab78f47233c8
snobbslakt <snobbslakt@d1ca7968-f502-11dd-9fdb-3debd5008f28>
2009-02-10 00:33:58 +0000

    started alphabeta

--- searchtree.cc
+++ searchtree.cc
@@ -1,5 +1,14 @@
 
+board alphabeta(board b, int depth, int alpha, int beta) {
+	
+	for(int i=0; i<32; i++) {
+	}
+	return b;
+}
 
-board minimax(board b, int depth) {
-   return b;
+int max(int a, int b) {
+	if(b > a)
+		return b;
+	else
+		return a;
 }
