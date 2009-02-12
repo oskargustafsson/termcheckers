94479af67fe6789038e14a4f93b3364d5c8e120f
linus.probert <linus.probert@d1ca7968-f502-11dd-9fdb-3debd5008f28>
2009-02-12 19:30:35 +0000

    Undo move added!

--- trunk/game.cc
+++ trunk/game.cc
@@ -5,127 +5,151 @@
 #include "searchtree.h"
 #include "board.h"
 #include "ui.h"
+#include <stack>
 
 using namespace std;
 
 namespace termcheckers {
 
-	Game::Game() {
-		compute_bits_in_char();
-		black_player = 0;
-		white_player = 0;
-		output = true;
-	}
-	Game::~Game() {
-		delete game;
-	}
+        Game::Game() {
+                compute_bits_in_char();
+                black_player = 0;
+                white_player = 0;
+                output = true;
+        }
+        Game::~Game() {
+                delete game;
+        }
 
-	bool Game::makeMove(unsigned int from, unsigned int to) {
-		if(!validateMove(b, from, to)) {
-			cout << "Illegal move.\n";
-			return false;
-		} else {
-			if(getCaptureMoves(b, from) == 0) {
-				move(b, from, to);
-			} else {
-				recursiveCapture(b, from, to);
-			}
-			
-			changePlayer(b);
-			cout << log2(from)+1 << "-" << log2(to)+1 << endl;
-			return true;
-		}
-	}
+        bool Game::makeMove(unsigned int from, unsigned int to) {
+                if(!validateMove(b, from, to)) {
+                        cout << "Illegal move.\n";
+                        return false;
+                } else {
+                  stackBoard();
+                        if(getCaptureMoves(b, from) == 0) {
+                                move(b, from, to);
+                        } else {
+                                recursiveCapture(b, from, to);
+                        }
 
-	bool Game::recursiveCapture(board tmpboard, unsigned int from, unsigned int to) {
-		unsigned int moves = getCaptureMoves(tmpboard, from);
-		unsigned int capture = 0x0;
-		board test;
-		while(moves != 0) {
-			capture = (moves & (moves-1)) ^ moves;
-			moves &= moves-1;
-			test = tmpboard;
-			move(test, from, capture);
+                        changePlayer(b);
+                        cout << log2(from)+1 << "-" << log2(to)+1 << endl;
+                        return true;
+                }
+        }
 
-			if(capture == to) {
-				b = test;
-				return true;
-			}
-			if(recursiveCapture(test, capture, to)) {
-				return true;
-			}
-		}
-		return false;
-	}
+        bool Game::recursiveCapture(board tmpboard, unsigned int from, unsigned int to) {
+                unsigned int moves = getCaptureMoves(tmpboard, from);
+                unsigned int capture = 0x0;
+                board test;
+                while(moves != 0) {
+                        capture = (moves & (moves-1)) ^ moves;
+                        moves &= moves-1;
+                        test = tmpboard;
+                        move(test, from, capture);
 
-	void Game::user() {
-		string line;
-		cin >> line;
+                        if(capture == to) {
+                                b = test;
+                                return true;
+                        }
+                        if(recursiveCapture(test, capture, to)) {
+                                return true;
+                        }
+                }
+                return false;
+        }
 
-		unsigned int from = 0x0;
-		unsigned int to = 0x0;
-		string first;
-		string second;
-		string::iterator It = line.begin();
-		int i=0;
+        void Game::user() {
+                string line;
+                cin >> line;
 
-		while( It != line.end()) {
-			if( *It == '-') {
-				i++;
-				It++;
-				continue;
-			}
-			if(i == 0)
-				first += *It;
-			if(i == 1)
-				second += *It;
-			It++;
-		}
-		from = pow(2.0, atof(first.c_str())-1);
-		to = pow(2.0, atof(second.c_str())-1);
+                if(line == "undo") {
+                  if(undoLastMove()) {
+                    cout << "Reverting!" << endl;
+                    return;
+                  } else {
+                    cout << "Nothing to undo!" << endl;
+                  }
+                }
 
-		if(!makeMove(from, to)) {
-			user();
-		}
-	}
+                unsigned int from = 0x0;
+                unsigned int to = 0x0;
+                string first;
+                string second;
+                string::iterator It = line.begin();
+                int i=0;
 
-	void Game::newGame(int black, int white, bool output) {
-		black_player = black;
-		white_player = white;
-		output = output;
-		b = createBoard();
+                while( It != line.end()) {
+                        if( *It == '-') {
+                                i++;
+                                It++;
+                                continue;
+                        }
+                        if(i == 0)
+                                first += *It;
+                        if(i == 1)
+                                second += *It;
+                        It++;
+                }
+                from = pow(2.0, atof(first.c_str())-1);
+                to = pow(2.0, atof(second.c_str())-1);
 
-	}
+                if(!makeMove(from, to)) {
+                        user();
+                }
+        }
 
-	void Game::play() {
+        void Game::newGame(int black, int white, bool output) {
+                black_player = black;
+                white_player = white;
+                output = output;
+                b = createBoard();
 
-		while(!endOfGame(b)) {
+        }
 
-			if(b.player == WHITE) {
-				depth = white_player;
-			}else if(b.player == BLACK) {
-				depth = black_player;
-			}
+        void Game::play() {
 
-			if(output) {
-				printBoard(b);
-				cout << ">> ";
-			}
+                while(!endOfGame(b)) {
 
-			if(depth > 0) {
-				alphabeta(b, depth);
-			} else {
-				user();
-			}
-		}
-	}
+                        if(b.player == WHITE) {
+                                depth = white_player;
+                        }else if(b.player == BLACK) {
+                                depth = black_player;
+                        }
 
-	Game* Game::game = NULL;
+                        if(output) {
+                                printBoard(b);
+                                cout << ">> ";
+                        }
 
-	Game* Game::instance() {
-		if(game == NULL) {
-			game = new Game();
-		}
-		return game;
-	}
+                        if(depth > 0) {
+                                alphabeta(b, depth);
+                        } else {
+                                user();
+                        }
+                }
+        }
+
+        Game* Game::game = NULL;
+
+        Game* Game::instance() {
+                if(game == NULL) {
+                        game = new Game();
+                }
+                return game;
+        }
+  bool Game::undoLastMove() {
+    if(history.empty()) {
+      return false;
+    } else {
+      history.pop();
+      b = history.top();
+      history.pop();
+    }
+  }
+
+  void Game::stackBoard() {
+    history.push(b);
+  }
 }
