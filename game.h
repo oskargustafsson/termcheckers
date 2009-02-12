82fe0454702834bca3d92f2e32131ac84da5b1ae
linus.probert <linus.probert@d1ca7968-f502-11dd-9fdb-3debd5008f28>
2009-02-12 19:30:35 +0000

    Undo move added!

--- game.h
+++ game.h
@@ -2,27 +2,32 @@
 #define GAME_H
 
 #include "board.h"
+#include <stack>
 
 namespace termcheckers {
 
-	class Game {
-	public:
-		~Game();
-		bool makeMove(unsigned int from, unsigned int to);
-		void newGame(int black, int white, bool output);
-		void play();
-		static Game* instance();
-		int depth;
-	private:
-		Game();
-		void user();
-		bool recursiveCapture(board tmpboard, unsigned int from, unsigned int to);
+        class Game {
+        public:
+                ~Game();
+                bool makeMove(unsigned int from, unsigned int to);
+                void newGame(int black, int white, bool output);
+                void play();
+                static Game* instance();
+                int depth;
+        private:
+                Game();
+                void user();
+                bool recursiveCapture(board tmpboard, unsigned int from, unsigned int to);
 
-		static Game* game;
-		board b;
-		int black_player;
-		int white_player;
-		bool output;
-	};
+                bool undoLastMove();
+                void stackBoard();
+
+                static Game* game;
+                std::stack<board> history;
+                board b;
+                int black_player;
+                int white_player;
+                bool output;
+        };
 }
 #endif
