b8463debd1635fe19076a4bd8bc430b46c6ad941
linus.probert <linus.probert@d1ca7968-f502-11dd-9fdb-3debd5008f28>
2009-02-17 16:19:48 +0000

    Gui updates, minor bugs

--- gui.h
+++ gui.h
@@ -9,22 +9,27 @@
 #define DEPTH 10
 
 namespace checkers {
-	class Game;
+        class Game;
 
-	class GUI {
-	public:
-		GUI(Game* g);
-		~GUI();
+        class GUI {
+        public:
+                GUI(Game* g);
+                ~GUI();
 
-		void printBoard(Board& b);
-		static void printInt(unsigned int b);
-		void input();
-		void println(std::string str);
-	private:
-		Game* game;
-		int isMovement(std::string line);
-		std::vector<unsigned int> parseMovement(std::string line);
-	};
+                void setInfo(std::string str, std::string inf);
+                void printBoard(Board& b);
+                static void printInt(unsigned int b);
+                void input();
+                void println(std::string str);
+        private:
+                Game* game;
+                int isMovement(std::string line);
+                std::vector<unsigned int> parseMovement(std::string line);
+
+                void clearScreen();
+
+                std::string lastMove;
+        };
 }
 
 #endif
