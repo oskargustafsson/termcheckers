0bc1dc3cc789aa0745c344e39332e9dcd10961da
snobbslakt <snobbslakt@d1ca7968-f502-11dd-9fdb-3debd5008f28>
2009-02-21 14:18:39 +0000

    buggfix in history heuristic

--- aitest.cc
+++ aitest.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <sstream>
+#include <vector>
 #include "gui.h"
 #include "board.h"
 #include "game.h"
@@ -12,6 +13,9 @@ using namespace std;
 
 int main() {
 
+	float depth = 0;
+	float nodes = 0;
+
 	Game* game = new Game();
 	GUI* gui = new GUI(game);
 	game->setGUI(gui);
@@ -21,14 +25,19 @@ int main() {
 
 	int i=0;
 	while(!game->board.endOfGame() && i < 100) {
-		gui->printBoard(game->board);
-		game->ai();
+		Search s(game);
+		s.search();
+		nodes += s.nrOfNodes;
+		nodes /= 2;
+		depth += s.maxdepth;
+		depth /= 2;
 		i++;
 	}
-	std::cout << "DONE" << std::endl;
+	cout << "DONE" << std::endl;
+	cout << "avg depth: " << depth << " avg nodes: " << nodes << "\n";
 
-	Search s(game);
-	s.sortTest();
+//	Search s(game);
+//	s.sortTest();
 
 	delete game;
 	delete gui;
