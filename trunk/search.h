116ed1f1340d905e7a9fb1f7caf5e9cfeb64d47b
snobbslakt <snobbslakt@d1ca7968-f502-11dd-9fdb-3debd5008f28>
2009-02-23 19:01:50 +0000

    fixed better time-management

--- trunk/search.h
+++ trunk/search.h
@@ -5,12 +5,9 @@
 #include "timer.h"
 #include <vector>
 
-#define MAX_TIME 1000000
-
 // DEFINE WHAT OPTIMIZATIONS TO USE:
 #define SCOUT
 #define HISTORY_HEURISTIC
-#define ITERATIVE_DEEPENING
 
 namespace checkers {
 
@@ -28,23 +25,15 @@ namespace checkers {
 			Search();
 			~Search();
 
-			SearchResult search(Board board);
-			void sortTest();
-
-			int time;
-			int maxdepth;
-			int nrOfNodes;
-			int extendedDepth;
+			SearchResult search(Board board, int time);
 		private:
 			int alphabeta(Board& b, int depth, int alpha, int beta);
 			int captureAlphaBeta(Board& b, int depth, int alpha, int beta, unsigned int from);
 			bool singleJump(Board b);
-			void reverse(std::vector<unsigned int>& list);
 			void newBestMove(Board& board, unsigned int from, unsigned int to);
-			void sortMovesHeap(unsigned int movelist[], int movevalues[], unsigned int movecount);
-			inline void siftDown(unsigned int movelist[], int movevalues[], int start, int end);
+			//inline void sortMovesHeap(unsigned int movelist[], int movevalues[], unsigned int movecount);
+			//inline void siftDown(unsigned int movelist[], int movevalues[], int start, int end);
 			inline void swap(unsigned int movelist[], int movevalues[], int a, int b);
-
 			inline void insertMove(unsigned int movelist[], int movevalues[], unsigned int from, unsigned int to, int newValue, unsigned int& movecount);
 
 			std::vector<unsigned int>* movement;
@@ -52,6 +41,13 @@ namespace checkers {
 
 			timer::Timer* timer;
 
+			int time;
+			int maxdepth;
+			int nrOfNodes;
+			int extendedDepth;
+			bool finished_search;
+			int time_check;
+			int max_time;
 #ifdef HISTORY_HEURISTIC
 			int history[32][32];
 #endif // HISTORY_HEURISTIC
